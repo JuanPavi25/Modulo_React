0dd923189e5b725f5b085813ba03e868b3032a83
JuanPavi25 <juancamilopavi9@gmail.com>
2025-11-28 00:01:52 -0500

    Challenge 1 finalizado

--- proyectos/00-challenge-1/src/App.jsx
+++ proyectos/00-challenge-1/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,29 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {<section className='d-flex flex-column justify-content-center'>
+        <h1 className='text-light'>Challenge 1: Contador Interactivo</h1>
+
+        <div className="card rounded-3 shadow-lg p-1">
+          <div className='card-body'>
+            <strong>{count}</strong>
+          </div>
+
+        </div>
+        <div>
+          <button onClick={() => setCount((count) => count + 1)}>
+            Aumentar Contador
+          </button>
+          <button onClick={() => setCount((count) => count - 1)}>
+            Disminuir Contador
+          </button>
+          <button onClick={() => setCount((count) => count = 0)}>
+            Resetear Contador
+          </button>
+        </div>
+
+      </section>
+      }
     </>
   )
 }
